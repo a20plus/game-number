615b0292a998b9542fe5128753d95514759798dc
Abolfazl <webabolfazl1837@gmail.com>
2025-09-19 10:36:52 +0330

    Create menu game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [boxSize, setBoxSize] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {boxSize ? <Game boxSize={boxSize} /> : <Menu setBoxSize={setBoxSize} />}
+    </div>
+  );
+}
+
+function Menu({ setBoxSize }) {
+  return (
+    <div>
+      <h2>Chosse your Box</h2>
+      <button value="3" onClick={(e) => Number(setBoxSize(e.target.value))}>
+        3*3
+      </button>
+      <button value="4" onClick={(e) => Number(setBoxSize(e.target.value))}>
+        4*4
+      </button>
+      <button value="5" onClick={(e) => Number(setBoxSize(e.target.value))}>
+        5*5
+      </button>
+    </div>
+  );
+}
+
+function Game({ boxSize }) {
+  const cells = Array.from({ length: boxSize * boxSize }, (b, i) => i);
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${boxSize}, 80px)`,
+        gap: "5px",
+        // border: "solid 0px #000",
+      }}
+    >
+      {cells.map((cell) => (
+        <div
+          key={cell}
+          style={{
+            width: "80px",
+            height: "80px",
+            backgroundColor: "#ddd",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {cell + 1}
+        </div>
+      ))}
     </div>
   );
 }
